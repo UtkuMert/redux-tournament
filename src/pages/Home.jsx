ba59564ab56ca3a7a8e32ab475e51c7cefccb1cb
UtkuMert <65029733+UtkuMert@users.noreply.github.com>
2022-06-03 07:28:44 +0300

    home page desing add image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
-import { HomeTeamList } from "../features/home/teams/HomeTeamList";
-import {HomeTournamentList} from "../features/home/tournaments/HomeTournamentList";
 
+import Background from '../img/top.jpg'
 export const Home = () => {
   return (
     <>
-      <p>Hello</p>
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: `url(${Background})`}}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">TURNUVAMVAR'A HOŞGELDİNİZ</h1>
+            <p className="mb-5">
+              Futbol tutkunlarının vazgeçilmez adresi TURNUVAMVAR. Kolaylıkla turnuva düzenleyebilir, turnuvayı yönetebilir ve turnuvanın gidişatını sitemiz üzerinden takip edebilirsiniz.
+            </p>
+            <button className="btn btn-primary">KAYIT OL</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
