2c623bda6e6bc2505735f7604ce746370cef4772
UtkuMert <65029733+UtkuMert@users.noreply.github.com>
2022-06-03 09:02:57 +0300

    fixed stageteam slice

--- src/features/admin/stageTeams/stageTeamsSlice.js
+++ src/features/admin/stageTeams/stageTeamsSlice.js
@@ -39,7 +39,34 @@ export const addTeamToStage = createAsyncThunk(
     }
   }
 );
-
+export const updateStageTeam = createAsyncThunk(
+  "/stageteam/update",
+  async (initialStageTeam) => {
+    try {
+      const { stageId } = initialStageTeam;
+      const response = await axios.put(
+        `/stageteam/update/${stageId}`,
+        initialStageTeam
+      );
+      return response?.data?.data;
+    } catch (err) {
+      return err.message;
+    }
+  }
+);
+export const deleteStageTeam = createAsyncThunk(
+  "/delete/{stageId}",
+  async (initialStageTeam) => {
+    const { stageId } = initialStageTeam;
+    try {
+      const response = await axios.delete(`/stageteam/delete/${stageId}`);
+      if (response?.status === 200) return initialStageTeam;
+      return `${response?.status}: ${response?.statusText}`;
+    } catch (err) {
+      return err.message;
+    }
+  }
+);
 const stageTeamsSlice = createSlice({
   name: "stageTeams",
   initialState,
@@ -58,12 +85,49 @@ const stageTeamsSlice = createSlice({
         state.status = "failed";
         state.error = action.error.message;
       })
+      .addCase(addTeamToStage.pending, (state, action) => {
+        state.status = "loading";
+      })
       .addCase(addTeamToStage.fulfilled, (state, action) => {
         console.log(action.payload);
         state.stageTeams.push(action.payload);
       })
       .addCase(addTeamToStage.rejected, (state, action) => {
-        state.error = action.payload.message
+        state.error = action.payload.message;
+      })
+      .addCase(updateStageTeam.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(updateStageTeam.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Update could not complete");
+          console.log(action.payload);
+          return;
+        }
+        const { id } = action.payload;
+        const stageTeams = state.stageTeams.filter(
+          (stageTeam) => stageTeam.id !== id
+        );
+        state.stageTeam = [...stageTeams, action.payload];
+        console.log(action.payload);
+      })
+      .addCase(updateStageTeam.rejected, (state, action) => {
+        state.error = action.payload.message;
+      })
+      .addCase(deleteStageTeam.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(deleteStageTeam.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Delete could not complete");
+          return;
+        }
+        const { id } = action.payload;
+        const stageTeams = state.stageTeams?.filter((stageTeam) => stageTeam?.id !== id);
+        state.stageTeams = stageTeams;
+      })
+      .addCase(deleteStageTeam.rejected, (state, action) => {
+        state.error = action.payload.message;
       });
   },
 });
@@ -73,6 +137,8 @@ export const getStageTeamsStatus = (state) => state?.stageTeams?.status;
 export const getStageTeamsError = (state) => state?.stageTeams?.error;
 
 export const selectStageTeamByStageId = (state, id) =>
-state?.stageTeams?.stageTeams?.filter((stageTeam) => stageTeam?.stageId === id)
+  state?.stageTeams?.stageTeams?.filter(
+    (stageTeam) => stageTeam?.stageId === id
+  );
 
 export default stageTeamsSlice.reducer;
